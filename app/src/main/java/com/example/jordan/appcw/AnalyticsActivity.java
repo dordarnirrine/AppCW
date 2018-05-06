c3b27909ad0fa6d646fabd3613ddef8ca96197d4
dordarnirrine <jordan.st.irvine@gmail.com>
2018-05-06 22:11:50 +0100

    Added Drop down to analytics.

--- app/src/main/java/com/example/jordan/appcw/AnalyticsActivity.java
+++ app/src/main/java/com/example/jordan/appcw/AnalyticsActivity.java
@@ -6,8 +6,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class AnalyticsActivity extends AppCompatActivity {
+public class AnalyticsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,28 @@ public class AnalyticsActivity extends AppCompatActivity {
 
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spinner1);
+        //create a list of items for the spinner.
+        String[] items = new String[]{"1", "2", "three"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+
+        dropdown.setOnItemSelectedListener(this);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 
 }
--- app/src/main/java/com/example/jordan/appcw/EditProfileActivity.java
+++ app/src/main/java/com/example/jordan/appcw/EditProfileActivity.java
@@ -1,11 +1,13 @@
 package com.example.jordan.appcw;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class EditProfileActivity extends AppCompatActivity {
 
@@ -16,15 +18,24 @@ public class EditProfileActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button doneButton = findViewById(R.id.doneButton);
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                done();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void done(){
+        Intent isEditingDoneIntent = new Intent( this,
+                DashActivity.class);
+
+        startActivity(isEditingDoneIntent);
     }
 
 }
